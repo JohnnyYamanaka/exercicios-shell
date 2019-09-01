da7a352d25c49485c5192481284ddf24b812f876
Johnny Yamanaka <yamanaka.johnny@outlook.com>
2019-09-01 12:08:17 -0300

    first commit

---
+++ ex1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear
+
+echo "informe o primeiro número \n"
+
+read var1
+
+echo "informe o segundo número \n"
+
+read var2
+
+echo "primerio número é $var1"
+echo "e o segundo número é $var2"
